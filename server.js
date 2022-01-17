e09956eedd06117df5b9d7408b69c5522698dd70
paperpatch <patchen21@gmail.com>
2022-01-17 17:32:36 -0500

    added pizza api routes

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static('public'));
 app.use(require('./routes'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
-  useNewUrlParse: true,
+  useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
