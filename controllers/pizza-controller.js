388fa47b683981805aaf9f1959c8e7c9297b8700
paperpatch <patchen21@gmail.com>
2022-01-18 14:56:47 -0500

    18.2

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -13,7 +13,7 @@ const pizzaController = {
       .then(dbPizzaData => res.json(dbPizzaData))
       .catch(err => {
         console.log(err);
-        res.status(400).json(err);
+        res.sendStatus(400);
       });
   },
 
@@ -25,16 +25,10 @@ const pizzaController = {
         select: '-__v'
       })
       .select('-__v')
-      .then(dbPizzaData => {
-        if (!dbPizzaData) {
-          res.status(404).json({ message: 'No pizza found with this id!' });
-          return;
-        }
-        res.json(dbPizzaData);
-      })
+      .then(dbPizzaData => res.json(dbPizzaData))
       .catch(err => {
         console.log(err);
-        res.status(400).json(err);
+        res.sendStatus(400);
       });
   },
 
@@ -55,20 +49,14 @@ const pizzaController = {
         }
         res.json(dbPizzaData);
       })
-      .catch(err => res.status(400).json(err));
+      .catch(err => res.json(err));
   },
 
   // delete pizza
   deletePizza({ params }, res) {
     Pizza.findOneAndDelete({ _id: params.id })
-      .then(dbPizzaData => {
-        if (!dbPizzaData) {
-          res.status(404).json({ message: 'No pizza found with this id!' });
-          return;
-        }
-        res.json(dbPizzaData);
-      })
-      .catch(err => res.status(400).json(err));
+      .then(dbPizzaData => res.json(dbPizzaData))
+      .catch(err => res.json(err));
   }
 };
 
--- server.js
+++ server.js
@@ -7,15 +7,16 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use(require('./routes'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
-  // useFindAndModify: false, // not supported in Mongoose 6.0
+  // useFindAndModify: false, // Mongoose 6.0 deprecated and no longer in use.
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
+
+app.use(require('./routes'));
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
